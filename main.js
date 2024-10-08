1bfcecba275366c402de5812b4187ae1924dba5f
Maggie Boyd <maggie.boyd@stu.ocsb.ca>
2024-10-08 12:08:49 -0400

    learnt value inputs and templates

--- main.js
+++ main.js
@@ -19,3 +19,46 @@ console.log(`I generated the number ${random_number}`)
 
 // ***  Your code goes below  ***
 
+//Part 1
+
+let celsius = 100
+let fahrenheit = 0
+
+//right below is the same as celsius = celsius + 2
+celsius += 2
+fahrenheit = (celsius * 9/5) + 32
+
+console.log(fahrenheit)
+
+//Part 2
+
+fahrenheit = 77
+celsius = (fahrenheit - 32) * 5/9
+console.log(celsius)
+
+//Part 3
+
+//the dollar sign is just apart of the {value} code
+console.log(`${fahrenheit} degrees Fahrenheit is ${celsius} degrees Celsius.`)
+
+//Part 4
+
+//A(-4,5)   B(6,0)
+m = (0 - 5)/(6 - -4)
+console.log(m)
+
+//Part 5
+
+let radius = Math.round(Math.random() * 10)
+let height = Math.round(Math.random() * 10)
+let volume = Math.round(Math.PI * radius**2 * height)
+
+console.log(`The random cylinder:
+    Height: ${height}
+    Radius: ${radius}
+    Volume: ${volume}`)
+
+
+
+
+
